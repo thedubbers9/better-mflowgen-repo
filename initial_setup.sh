ab323ce213e8167ce192fc831db4d5a202b7d9a1
Eric Dubberstein <eric@dubberstein.com>
2025-10-03 17:52:57 -0400

    fix for initial setup

--- initial_setup.sh
+++ initial_setup.sh
@@ -7,6 +7,8 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p "$(pwd)/miniconda3"
 source mflowgen_env_setup_script_$1.sh
 conda create --name mflowgen_env python=3.8 ipython
+
+source "$(pwd)/miniconda3/bin/activate" mflowgen_env
 conda install pyyaml
 if [ "$1" == "stanford" ]; then
     conda install -c conda-forge libxss
